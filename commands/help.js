b2e3e9e15e39cbf7e84cf9670a3300fbcf229515
Khana9 <abdul.khan9@mail.dcu.ie>
2021-11-04 20:33:20 +0000

    adds the help list

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,15 @@
+const { MessageEmbed } = require("discord.js");
 exports.run = async (client, message, args) => {
     
     
-    message.lineReply("Kek, you think im going to help you? :laughing:")
 
-}
+        let embed = new MessageEmbed()
+        .setTitle("Command List")
+        .setDescription(`**-help:**\nThis command.\n**-rolldice:**\nRolls a dice\n**-hangman.(custom.random):**\nStarts a custom/random hangman game\n**-hello:**\nReplies with hello :)\n**-bye:**\nReplies with goodbye!\n**-kms:**\nGives you a quick tip :D\n**-meme:**\nSends a meme\n**-ping**\nGives the bot latency\n**-yo**\nReplies to you\n**MODERATOR COMMANDS:**\n**-kick:**\nKicks a user\n**-ban**\nBans a user\n**-mute**\nMutes a user`)
+        .setColor("RANDOM")
+        message.channel.send(embed)
+
+    }
 
 exports.help = {
     name: "help"
