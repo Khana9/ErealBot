862d5e7b8af7dca0dfcf7a1fda982e11761a195d
Khana9 <abdul.khan9@mail.dcu.ie>
2021-11-12 18:39:25 +0000

    add a special help page. suggested by paulinaad2

---
+++ commands/plshelp.js
@@ -0,0 +1,9 @@
+exports.run = async (client,message,args) => {
+        
+    message.lineReply("Sure, here you go!: https://www.dcu.ie/students/withdrawing-university")
+     
+}
+
+exports.help = {
+    name: "plshelp"
+}
