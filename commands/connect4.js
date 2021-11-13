86911d6a270f02b2cddc0607a5d02ab926336512
Calcifer2002 <83289730+Calcifer2002@users.noreply.github.com>
2021-11-14 04:59:40 +0530

    Adds connect4.js

---
+++ commands/connect4.js
@@ -0,0 +1,23 @@
+const { ConnectFour } = require('djs-games')
+module.exports = new Command({
+  name: "Connect4",
+  description: "Play connect 4!",
+  async run(message, args, client) {
+    const game = new GTF({
+      message: message,
+      token: "",
+      stopCommand: "stop",
+      winFooter: "You Win!",
+      winColor: "GREEN",
+      loseFooter: "You Lose!",
+      loseColor: "RED",
+      questionFooter: "Guess the Flag!",
+      questionColor: "BLUE",
+      winMessage: "You Win!",
+      loseMessage: "You Lose!",
+      maxAttempts: 5,
+      wrongGuess: "Wrong Guess!",
+    });
+    game.start();
+  },
+});
