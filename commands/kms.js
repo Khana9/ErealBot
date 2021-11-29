972a60126a17e67ab70eb7de924314ff2d1e4010
Khana9 <abdul.khan9@mail.dcu.ie>
2021-11-29 22:53:53 +0000

    fix kms not working on v13

--- commands/kms.js
+++ commands/kms.js
@@ -6,6 +6,6 @@ module.exports = new Command({
 	name: "kms",
 	description: "A small motivational line :)",
     async run(message, args, client) {
-    message.lineReply("If you are stuck, Dont worry! Take a break and drink some coffee :-)")
+    message.reply("If you are stuck, Dont worry! Take a break and drink some coffee :-)")
 }
 })
